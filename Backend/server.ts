2355c6b4cf4ec08aa290d315d60190539e4bebdd
arandombird671 <arandombird671@gmail.com>
2024-08-29 11:58:19 +0300

    / path

--- Backend/server.ts
+++ Backend/server.ts
@@ -9,6 +9,9 @@ const socketEvents = require("./socket/socket.ts");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req: any, res: any) => {
+  res.json({});
+});
 io.on("connection", socketEvents.onConnection);
 
 server.listen(port, () => {
